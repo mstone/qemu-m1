b51cb72064e20951bef7e75c6934b82a1f8b0bd8
Michael Stone <m@mstone.info>
2021-09-14 11:02:31 -0400

    nix: remove qemuSrc hack now that nix#4423 is fixed

--- flake.nix
+++ flake.nix
@@ -3,18 +3,11 @@
 
   inputs.utils.url = "github:numtide/flake-utils";
 
-  # note: until https://github.com/NixOS/nix/issues/4423 is fixed, we need a separate qemuSrc repo since qemu uses submodules
-  inputs.qemuSrc.url = "https://github.com/mstone/qemu-m1";
-  inputs.qemuSrc.type = "git";
-  inputs.qemuSrc.ref = "m1-test";
-  inputs.qemuSrc.submodules = true;
-  inputs.qemuSrc.flake = false;
-
   inputs.sigtool.url = "github:mstone/sigtool";
   inputs.sigtool.inputs.nixpkgs.follows = "nixpkgs";
   inputs.sigtool.inputs.utils.follows = "utils";
 
-  outputs = { self, nixpkgs, utils, qemuSrc, sigtool }: let
+  outputs = { self, nixpkgs, utils, sigtool }: let
     name = "qemu";
     version = "6.1.0";
   in utils.lib.simpleFlake {
@@ -27,7 +20,7 @@
       qemu = with final; rec {
         qemu = (prev.qemu.overrideAttrs (old: rec {
           inherit version;
-          src = qemuSrc;
+          src = self;
           patches = [(builtins.head (builtins.tail old.patches))];
           buildInputs = old.buildInputs ++ [ libtasn1 ];
           nativeBuildInputs = [ sigtool.defaultPackage.${prev.system} (python39.withPackages (ps: with ps; [sphinx sphinx_rtd_theme])) ] ++ (lib.drop 2 old.nativeBuildInputs);
